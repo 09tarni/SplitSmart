a514422aa1164183ad1a2160c45ca49f2f83a79f
09tarni <tarnikhatri55@gmail.com>
2026-06-17 22:14:46 +0530

    fix: use REACT_APP_API_URL in socket connection

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+const socket = io(BASE_URL, {
   autoConnect: false,
   reconnection: true,
   reconnectionAttempts: 5,
